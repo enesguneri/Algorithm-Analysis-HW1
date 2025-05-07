a0cec5ebe0d7278fa3fe527b1a7b1e8c9f87ca80
enesguneri <enesarda62@hotmail.com>
2025-05-07 21:06:38 +0300

    Improved code structure

--- src/Sorting.java
+++ src/Sorting.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Sorting {
@@ -8,80 +9,57 @@ public class Sorting {
         File file100K = new File("100K_random_input.txt");
 
         for (int i = 1; i <= 4; i++) {
-            sortSelection(i,file1K,1000,true,true);//Reading numbers from file, and sorting array after creating random numbers txt.
-            sortSelection(i,file1K,1000,false,false);
-            sortSelection(i,file10K,10000,true,true);
-            sortSelection(i,file10K,10000,false,false);
-            sortSelection(i,file100K,100000,true,true);
-            sortSelection(i,file100K,100000,false,false);
+            sortSelection(i,file1K,1000,"random");
+            sortSelection(i,file1K,1000,"increasing");
+            sortSelection(i,file1K,1000,"decreasing");
+
+            sortSelection(i,file10K,10000,"random");
+            sortSelection(i,file10K,10000,"increasing");
+            sortSelection(i,file10K,10000,"decreasing");
+
+            sortSelection(i,file100K,100000,"random");
+            sortSelection(i,file100K,100000,"increasing");
+            sortSelection(i,file100K,100000,"decreasing");
         }
 
     }
 
-    public static void sortSelection(int i,File file,int size,boolean isIncreasing,boolean isRandom) {
+
+    public static void sortSelection(int i,File file,int size,String dataType) {
         int[] arr = new int[size];
-
-        if (isRandom) {
-            String arraySize = "";
-            if (size == 1000)
-                arraySize = "1K";
-            else if (size == 10000)
-                arraySize = "10K";
-            else if (size == 100000)
-                arraySize = "100K";
-
-            long startTime = System.nanoTime();
-            readFileToArr(arr, file);
-            System.out.println(arraySize + " array has been assigned from file in " + calculateRunningTime(startTime) + " ms");
-
-            String sortName = "";
-            switch (i) {
-                case 1:
-                    sortName = "insertionsort";
-                    break;
-                case 2:
-                    sortName = "mergesort";
-                    break;
-                case 3:
-                    sortName = "heapsort";
-                    break;
-                case 4:
-                    sortName = "quicksort";
-                    break;
-                default:
-                    break;
-
-            }
-
-            String fileName = sortName + "_" + arraySize + "_" + "random_output.txt";
-            writeArrToFile(arr, fileName);
-
+        readFileToArr(arr, file);
+        if (dataType.equals("increasing")) {
+            Arrays.sort(arr);
         }
-        else
-            readFileToArr(arr, file);
-        long startTime = System.nanoTime();
+        else if (dataType.equals("decreasing")) {
+            decreasingQuickSort(arr, 0, arr.length - 1);
+            //Arrayi azalan sırada sıralayabilmek için quickSortun azalan sırada sıralayan hali yazıldı.
+        }
+
         String sortName = "";
+        long startTime = System.nanoTime();
         switch (i) {
             case 1:
-                insertionSort(arr, isIncreasing);
+                insertionSort(arr);
                 sortName = "insertionsort";
                 break;
             case 2:
-                mergeSort(arr, 0, arr.length - 1, isIncreasing);
+                mergeSort(arr, 0, arr.length - 1);
                 sortName = "mergesort";
                 break;
             case 3:
-                heapSort(arr, arr.length - 1, isIncreasing);
+                heapSort(arr, arr.length);
                 sortName = "heapsort";
                 break;
             case 4:
-                quickSort(arr, 0, arr.length - 1, isIncreasing);
+                quickSort(arr, 0, arr.length - 1);
                 sortName = "quicksort";
                 break;
             default:
                 break;
-
         }
+        double runningTime = calculateRunningTime(startTime);
+
         String arraySize = "";
         if (size == 1000)
             arraySize = "1K";
@@ -89,11 +67,9 @@ public class Sorting {
             arraySize = "10K";
         else if (size == 100000)
             arraySize = "100K";
-        System.out.println(arraySize + " array has been sorted in " + (isIncreasing ? "increasing" : "decreasing") + " order with " + sortName + " in " + calculateRunningTime(startTime) + " ms");
-
-        String fileName = sortName + "_" + arraySize + "_" + (isIncreasing ? "increasing" : "decreasing") + "_output" +".txt";
-
-        writeArrToFile(arr, fileName);
+        System.out.println(arraySize + " array of " + dataType + " integers has been sorted with " + sortName + " in " + runningTime + " ms");
+        String fileName = sortName + "_" + arraySize + "_" + (dataType) + "_output.txt";
+        writeArrToFile(arr,fileName);
     }
 
     public static void readFileToArr(int[] arr, File file) {
@@ -108,7 +84,7 @@ public class Sorting {
 
     }
 
-    public static void writeArrToFile(int[] arr, String fileName) {
+    public static void writeArrToFile(int[] arr,String fileName) {
         try(BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName))) {
             for(int num : arr) {
                 bufferedWriter.write(String.valueOf(num));
@@ -124,12 +100,12 @@ public class Sorting {
         return (double) (endTime - startTime) /1000000.0;
     }
 
-    public static void insertionSort(int[] array, boolean isIncreasing) {
+    public static void insertionSort(int[] array) {
         for (int i = 1; i < array.length; i++) {
             int key = array[i];
             int j = i - 1;
 
-            while (j >= 0 && (isIncreasing ? array[j] > key : array[j] < key)) {
+            while (j >= 0 && (array[j] > key)) {
                 array[j + 1] = array[j];
                 j--;
             }
@@ -141,19 +117,19 @@ public class Sorting {
         //Worst Case : O(n^2)
     }
 
-    public static void mergeSort(int[] array, int p, int r, boolean isIncreasing) {
+    public static void mergeSort(int[] array, int p, int r) {
         if(p < r) {
             int q = (p + r) / 2;
-            mergeSort(array, p, q, isIncreasing);
-            mergeSort(array, q + 1, r , isIncreasing);
-            merge(array, p, q, r, isIncreasing);
+            mergeSort(array, p, q);
+            mergeSort(array, q + 1, r);
+            merge(array, p, q, r);
         }
         //Best Case : O(nlogn)
         //Average Case : O(nlogn)
         //Worst Case : O(nlogn)
     }
 
-    private static void merge(int[] array, int p, int q, int r, boolean isIncreasing) {
+    private static void merge(int[] array, int p, int q, int r) {
         int n1 = q - p + 1;
         int n2 = r - q;
         int[] leftArr = new int[n1];
@@ -166,7 +142,7 @@ public class Sorting {
         }
         int i = 0, j = 0, k = p;
         while (i < n1 && j < n2) {
-            if (isIncreasing ? leftArr[i] <= rightArr[j] : leftArr[i] >= rightArr[j])  {//gönderilen boolean değerine göre artan veya azalan sırada sıralanır.
+            if (leftArr[i] <= rightArr[j])  {//gönderilen boolean değerine göre artan veya azalan sırada sıralanır.
                 array[k++] = leftArr[i++];
             } else {
                 array[k++] = rightArr[j++];
@@ -180,22 +156,22 @@ public class Sorting {
         }
     }
 
-    public static void quickSort(int[] array, int p, int r, boolean isIncreasing) {
+    public static void quickSort(int[] array, int p, int r) {
         if (p < r) {
-            int q = quickSortPartition(array, p, r, isIncreasing);
-            quickSort(array, p, q - 1, isIncreasing);
-            quickSort(array, q + 1, r, isIncreasing);
+            int q = quickSortPartition(array, p, r);
+            quickSort(array, p, q - 1);
+            quickSort(array, q + 1, r);
         }
         //Best Case: O(nlogn)
         //Average Case: O(nlogn)
         //Worst Case: O(n^2)
     }
 
-    private static int quickSortPartition(int[] array, int p, int r, boolean isIncreasing) {
+    private static int quickSortPartition(int[] array, int p, int r) {
         int x = array[r];//son eleman pivot olarak seçilir.
         int i = p - 1;
         for (int j = p; j < r; j++) {
-            if (isIncreasing ? array[j] <= x : array[j] >= x) {
+            if (array[j] <= x) {
                 i++;
                 int temp = array[i];
                 array[i] = array[j];
@@ -208,16 +184,16 @@ public class Sorting {
         return i + 1;
     }
 
-    public static void heapSort(int[] array, int n, boolean isIncreasing) {
+    public static void heapSort(int[] array, int n) {
         for(int i = n/2 - 1; i >= 0; i--){
-            heapify(array, i, n, isIncreasing);
+            heapify(array, i, n);
         }
-        for(int i = n; i >= 0; i--){
+        for(int i = n-1; i >= 0; i--){
             int temp = array[0];
             array[0] = array[i];
             array[i] = temp;
 
-            heapify(array, 0, i, isIncreasing);
+            heapify(array, 0, i);
         }
 
         //Best Case: O(nlogn)
@@ -225,23 +201,48 @@ public class Sorting {
         //Worst Case: O(nlogn)
     }
 
-    private static void heapify(int[] array, int i, int n, boolean isIncreasing) {
+    private static void heapify(int[] array, int i, int n) {
         int leftIndex = 2 * i + 1;//sol ve sağ çocuğun indexi belirlenir
         int rightIndex = 2 * i + 2;
         int largest = i;
 
-        if(leftIndex < n && (isIncreasing ? array[leftIndex] > array[largest] : array[leftIndex] < array[largest])){
+        if(leftIndex < n && (array[leftIndex] > array[largest])){
             largest = leftIndex;
         }
-        if(rightIndex < n && (isIncreasing ? array[rightIndex] > array[largest] : array[rightIndex] < array[largest])) {
+        if(rightIndex < n && (array[rightIndex] > array[largest])) {
             largest = rightIndex;
         }
         if(largest != i){
             int temp = array[largest];
             array[largest] = array[i];
             array[i] = temp;
-            heapify(array, largest, n, isIncreasing);
+            heapify(array, largest, n);
         }
+    }
+
+    public static void decreasingQuickSort(int[] array, int p, int r) {
+        if (p < r) {
+            int q = decreasingQuickSortPartition(array, p, r);
+            decreasingQuickSort(array, p, q - 1);
+            decreasingQuickSort(array, q + 1, r);
+        }
+    }
+
+    private static int decreasingQuickSortPartition(int[] array, int p, int r) {
+        int x = array[r];
+        int i = p - 1;
+        for (int j = p; j < r; j++) {
+            if (array[j] >= x) {
+                i++;
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+        int temp = array[i + 1];
+        array[i + 1] = array[r];
+        array[r] = temp;
+        return i + 1;
     }
 
 }
